651729aa2f2108bd92958409bd27d7b9d5be2ec5
Bravim-Ketan-Purohit <92784033+Bravim-Ketan-Purohit@users.noreply.github.com>
2022-11-23 14:27:35 +0530

    Update index.js

--- Task-3/index.js
+++ Task-3/index.js
@@ -5,11 +5,11 @@ const calculatetemp =()=>{
     // console.log(valueTemp)
 
     const celToFah = (celcius) => {
-        let f = Math.round((celcius*9/5)+32);
+        let f = (celcius*9/5)+32;
         return f;
     }
     const fahToCel = (fahrenheit) => {
-      let c = Math.round((5/9)*fahrenheit-32);
+      let c = (5/9)*fahrenheit-32;
       return c;
     };
 
